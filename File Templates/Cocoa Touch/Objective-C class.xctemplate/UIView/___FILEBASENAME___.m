a89434a4cc901fb7a207c1f9ca0c00759d634d4c
Yusuke SUGAMIYA <yusuke.dnpp@gmail.com>
2013-10-04 23:19:16 +0900

    余計なテンプレートを一掃した

--- File Templates/Cocoa Touch/Objective-C class.xctemplate/UIView/___FILEBASENAME___.m
+++ File Templates/Cocoa Touch/Objective-C class.xctemplate/UIView/___FILEBASENAME___.m
@@ -1,31 +1,9 @@
-//
-//  ___FILENAME___
-//  ___PROJECTNAME___
-//
-//  Created by ___FULLUSERNAME___ on ___DATE___.
-//___COPYRIGHT___
-//
-
 #import "___FILEBASENAME___.h"
 
+@interface ___FILEBASENAMEASIDENTIFIER___ ()
+
+@end
+
 @implementation ___FILEBASENAMEASIDENTIFIER___
-
-- (id)initWithFrame:(CGRect)frame
-{
-    self = [super initWithFrame:frame];
-    if (self) {
-        // Initialization code
-    }
-    return self;
-}
-
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect
-{
-    // Drawing code
-}
-*/
 
 @end
--- File Templates/Cocoa Touch/Objective-C test case class.xctemplate/SenTestCase/___FILEBASENAME___.m
+++ File Templates/Cocoa Touch/Objective-C test case class.xctemplate/SenTestCase/___FILEBASENAME___.m
@@ -1,11 +1,3 @@
-//
-//  ___FILENAME___
-//  ___PROJECTNAME___
-//
-//  Created by ___FULLUSERNAME___ on ___DATE___.
-//___COPYRIGHT___
-//
-
 #import <SenTestingKit/SenTestingKit.h>
 
 @interface ___FILEBASENAMEASIDENTIFIER___ : ___VARIABLE_testSubclass___
