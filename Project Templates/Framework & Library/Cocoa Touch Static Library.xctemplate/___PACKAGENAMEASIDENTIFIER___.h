e87e578799c817cadcecea5238b7768a725cea80
Yusuke SUGAMIYA <yusuke.dnpp@gmail.com>
2013-10-06 19:03:34 +0900

    プロジェクト生成時に付く余計なコメントを除去

--- Project Templates/Framework & Library/Cocoa Touch Static Library.xctemplate/___PACKAGENAMEASIDENTIFIER___.h
+++ Project Templates/Framework & Library/Cocoa Touch Static Library.xctemplate/___PACKAGENAMEASIDENTIFIER___.h
@@ -1,11 +1,3 @@
-//
-//  ___FILENAME___
-//  ___PACKAGENAME___
-//
-//  Created by ___FULLUSERNAME___ on ___DATE___.
-//___COPYRIGHT___
-//
-
 #import <Foundation/Foundation.h>
 
 @interface ___FILEBASENAMEASIDENTIFIER___ : NSObject
--- Project Templates/Framework & Library/Cocoa Touch Static Library.xctemplate/___PACKAGENAMEASIDENTIFIER___.m
+++ Project Templates/Framework & Library/Cocoa Touch Static Library.xctemplate/___PACKAGENAMEASIDENTIFIER___.m
@@ -1,11 +1,3 @@
-//
-//  ___FILENAME___
-//  ___PACKAGENAME___
-//
-//  Created by ___FULLUSERNAME___ on ___DATE___.
-//___COPYRIGHT___
-//
-
 #import "___FILEBASENAME___.h"
 
 @implementation ___FILEBASENAMEASIDENTIFIER___
